d596d50f142fe8c1368aa0e3f8907e755f5580ec
Jonaskop44 <jonas@codeflexx.com>
2024-07-09 21:56:33 +0200

    changes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
-    <div className="w-full overflow-x-hidden">
+    <main className="w-full overflow-x-hidden">
       <section className="relative h-[550px] overflow-hidden pb-5 pt-20 lg:h-[90vh]">
         <div className="absolute -left-8 bottom-0 -z-50 h-[120%] w-[130%] origin-top-left -rotate-6 rounded-[3rem] bg-sky-500 bg-[url('/images/start-background.webp')] bg-[length:auto_75%] bg-[-160px_140%] bg-no-repeat" />
         <div className="flex h-full w-full flex-col items-center justify-around pb-10 text-center">
@@ -20,12 +21,12 @@ const Home = () => {
               href={""}
               className="mt-8 inline-block transform rounded-full bg-blue-500 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
             >
-              Mehr Erfahren
+              Musikanlage auswählen
             </Link>
           </div>
         </div>
       </section>
-    </div>
+    </main>
   );
 };
 
--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,8 +1,76 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { BsList } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const toggleSidebar = (active: boolean) => {
+    setSidebar(active == null ? !sidebar : active);
+  };
+
+  const links = [
+    { name: "Start", page: "start" },
+    { name: "Musikanlagen", page: "musikanlagen" },
+    { name: "Lichttechnik", page: "lichttechnik" },
+    { name: "DJ-Equipment", page: "dj-equipment" },
+    { name: "Party-Sets", page: "party-sets" },
+  ];
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header className="absolute z-50 flex h-20 w-full items-center justify-between px-6 text-white lg:px-[5%] xl:px-[10%]">
+      <Link href="/" className="flex h-3/5 items-center justify-between gap-4">
+        <Image
+          src="/images/Logo_ELBE-1-749x800.png"
+          alt="RentYourBeat Logo"
+          width={100}
+          height={100}
+          sizes="100vw"
+          className="h-3/4 xl:h-full"
+        />
+        <h1 className="text-2xl font-bold xl:text-4xl">Elbe - Technik</h1>
+      </Link>
+      <nav>
+        <ul className="hidden space-x-4 lg:flex">
+          {links.map((link) => (
+            <li key={link.page}>
+              <Link
+                href={`/${link.page}`}
+                className={`delay font-normal transition-colors duration-150 ease-in-out hover:text-gray-300 `}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        {sidebar && (
+          <div className="fixed right-0 top-0 z-50 flex h-lvh w-full flex-col items-start justify-start bg-black bg-opacity-20 pt-20 backdrop-blur md:w-60 lg:hidden">
+            {links.map((link) => (
+              <Link key={link.page} href={`/${link.page}`}>
+                <a
+                  className="w-full pb-1.5 pl-5 pt-1.5 text-xl font-semibold transition-colors duration-150 ease-in-out hover:bg-black hover:bg-opacity-10 hover:text-gray-300"
+                  onClick={() => toggleSidebar(false)}
+                >
+                  {link.name}
+                </a>
+              </Link>
+            ))}
+          </div>
+        )}
+      </nav>
+      <BsList
+        name={sidebar ? "mingcute:close-fill" : "mingcute:menu-fill"}
+        size="32px"
+        className={`right-6 transition-colors duration-150 ease-in-out lg:!hidden ${
+          sidebar ? "fixed z-50" : ""
+        }`}
+        onClick={() => toggleSidebar(false)}
+      />
+    </header>
   );
 };
 
