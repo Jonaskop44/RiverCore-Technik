3e938071d79247cf8652bbbe8eef92fb3646c2e8
Jonaskop44 <jonas@codeflexx.com>
2024-07-10 22:45:35 +0200

    changes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -16,7 +16,7 @@ const navigation = [
 
 const Home = () => {
   return (
-    <div className="relative bg-green-500 overflow-hidden h-screen">
+    <div className="relative bg-white overflow-hidden h-screen">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
@@ -30,7 +30,7 @@ const Home = () => {
           </svg>
 
           <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8 h-[50px]">
               <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
@@ -115,7 +115,7 @@ const Home = () => {
             </Transition>
           </Popover>
 
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 lg:h-screen">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Innovationen nutzen</span>{" "}
@@ -124,7 +124,7 @@ const Home = () => {
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Bei <b>Just - Technik</b> bieten wir maßgeschneiderte
+                Bei <b>Elbe - Technik</b> bieten wir maßgeschneiderte
                 Techniklösungen für Ihr Unternehmen. Kontaktieren Sie uns per
                 Telefon oder E-Mail und profitieren Sie von unserer umfassenden
                 Beratung und Unterstützung. Setzen Sie Ihre Projekte mit
@@ -141,7 +141,7 @@ const Home = () => {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 w-full lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
