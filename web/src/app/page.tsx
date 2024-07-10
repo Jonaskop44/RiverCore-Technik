d9022e0aa669f7bcc4d8c35afecdef99148e67de
Jonaskop44 <jonas@codeflexx.com>
2024-07-10 20:43:23 +0200

    changes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,35 +1,152 @@
-import Link from "next/link";
-import Navbar from "@/components/Navbar";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { BsList } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+import { Button, ButtonGroup } from "@nextui-org/button";
+
+const navigation = [
+  { name: "Start", href: "#" },
+  { name: "Dienstleistung", href: "#" },
+  { name: "Team", href: "#" },
+  { name: "Placeholder", href: "#" },
+];
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="w-full overflow-x-hidden">
-        <section className="relative h-[550px] overflow-hidden pb-5 pt-20 lg:h-[90vh]">
-          <div className="absolute -left-8 bottom-0 -z-50 h-[120%] w-[130%] origin-top-left -rotate-6 rounded-[3rem] bg-sky-500 bg-[url('/images/start-background.webp')] bg-[length:auto_75%] bg-[-160px_140%] bg-no-repeat" />
-          <div className="flex h-full w-full flex-col items-center justify-around pb-10 text-center">
-            <div className="flex flex-col items-center gap-6 px-6 font-medium text-white">
-              <h2 className="text-3xl md:text-4xl">
-                Innovationen <b>nutzen</b>, erfolgreich <b>umsetzen</b>!
-              </h2>
-              <p className="w-11/12 text-lg md:w-3/5 md:text-xl lg:w-2/5">
+    <div className="relative bg-white overflow-hidden h-screen">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-black transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          <Popover>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="#">
+                      <span className="sr-only">Workflow</span>
+                      <img
+                        className="h-8 w-auto sm:h-10"
+                        src="/images/Logo_ELBE-1-749x800.png"
+                      />
+                    </a>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
+                        <span className="sr-only">Open main menu</span>
+                        <BsList className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </nav>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        alt=""
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
+                        <span className="sr-only">Close main menu</span>
+                        <IoMdClose className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="px-2 pt-2 pb-3 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <a
+                    href="#"
+                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Innovationen nutzen</span>{" "}
+                <span className="block text-indigo-600 xl:inline">
+                  erfolgreich umsetzen
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Bei <b>Elbe - Technik</b> bieten wir maßgeschneiderte
                 Techniklösungen für Ihr Unternehmen. Kontaktieren Sie uns per
                 Telefon oder E-Mail und profitieren Sie von unserer umfassenden
                 Beratung und Unterstützung. Setzen Sie Ihre Projekte mit
                 modernster Technik erfolgreich um!
               </p>
-              <Link
-                href={""}
-                className="mt-8 inline-block transform rounded-full bg-blue-500 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
-              >
-                Mehr Erfahren
-              </Link>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <Button className="w-full flex items-center justify-center px-8 py-6 text-base font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 md:py-8 md:text-lg md:px-10">
+                    Mehr Erfahren
+                  </Button>
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          </main>
+        </div>
       </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          alt=""
+        />
+      </div>{" "}
     </div>
   );
 };
