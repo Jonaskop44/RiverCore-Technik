b5d0c07dd4cdd272a36f9bfdd99902e36574dd1f
Jonaskop44 <jonas@codeflexx.com>
2024-07-11 22:54:49 +0200

    changes

--- web/src/app/team/page.tsx
+++ web/src/app/team/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import * as React from "react";
 import { motion, useCycle } from "framer-motion";
 
 /**
@@ -5,22 +8,36 @@ import { motion, useCycle } from "framer-motion";
  */
 
 const App = () => {
-  const [x, cycleX] = useCycle(0, "calc(3 * var(--width))");
+  const [width, nextWidth] = useCycle("0", "100%", "calc(50% + 100px)");
 
   return (
-    <motion.div
-      initial={false}
-      animate={{ x }}
-      transition={{ duration: 5, ease: () => 0.5 }}
-      style={{
-        width: 100,
-        height: 100,
-        background: "white",
-        "--width": "100px",
-      }}
-      onClick={() => cycleX()}
-    />
+    <div style={stretch} onClick={() => nextWidth()} className="bg-black">
+      <motion.div
+        initial={false}
+        animate={{ width }}
+        transition={{ duration: 5 }}
+        style={style}
+      />
+    </div>
   );
+};
+
+const style = {
+  width: 100,
+  height: 100,
+  background: "white",
+};
+
+const stretch: React.CSSProperties = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  inset: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 };
 
 export default App;
