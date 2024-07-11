ffd13a49274607a034bef7781ba57b351b559150
Jonaskop44 <jonas@codeflexx.com>
2024-07-11 09:31:01 +0200

    changes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,6 +6,8 @@ import { Popover, Transition } from "@headlessui/react";
 import { BsList } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 import { Button } from "@nextui-org/button";
+import { motion } from "framer-motion";
+import ParallaxText from "@/components/ParallaxText";
 
 const navigation = [
   { name: "Start", href: "#" },
@@ -16,138 +18,157 @@ const navigation = [
 
 const Home = () => {
   return (
-    <div className="relative bg-white overflow-hidden h-screen">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-
-          <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8 h-[50px]">
-              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                      <span className="sr-only">Workflow</span>
-                      <img
-                        className="h-8 w-auto sm:h-10"
-                        src="/images/Logo_ELBE-1-749x800.png"
-                        alt="Logo Elbe Technik"
-                      />
-                    </a>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 outline-none">
-                        <span className="sr-only">Open main menu</span>
-                        <BsList className="h-6 w-6" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="font-medium text-gray-500 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </nav>
-            </div>
-
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+    <div>
+      {/*Hero*/}
+      <main className="relative bg-white overflow-hidden h-screen">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <svg
+              className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
             >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 outline-none">
-                        <span className="sr-only">Close main menu</span>
-                        <IoMdClose className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
+              <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <Popover>
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8 h-[50px]">
+                <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                      <a href="#">
+                        <span className="sr-only">Workflow</span>
+                        <img
+                          className="h-8 w-auto sm:h-10"
+                          src="/images/Logo_ELBE-1-749x800.png"
+                          alt="Logo Elbe Technik"
+                        />
+                      </a>
+                      <div className="-mr-2 flex items-center md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 outline-none">
+                          <span className="sr-only">Open main menu</span>
+                          <BsList className="h-6 w-6" />
+                        </Popover.Button>
+                      </div>
                     </div>
                   </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
+                  <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        className="font-medium text-gray-500 hover:text-gray-900"
                       >
                         {item.name}
                       </a>
                     ))}
                   </div>
-                  <a
-                    href="#"
-                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
+                </nav>
+              </div>
 
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 lg:h-screen">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Innovationen nutzen</span>{" "}
-                <span className="block text-blue-600 xl:inline">
-                  erfolgreich umsetzen
-                </span>
-              </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Bei <b>Elbe - Technik</b> bieten wir maßgeschneiderte
-                Techniklösungen für Ihr Unternehmen. Kontaktieren Sie uns per
-                Telefon oder E-Mail und profitieren Sie von unserer umfassenden
-                Beratung und Unterstützung. Setzen Sie Ihre Projekte mit
-                modernster Technik erfolgreich um!
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <Button className="w-full flex items-center justify-center px-8 py-6 text-base font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 md:py-8 md:text-lg md:px-10">
-                    Mehr Erfahren
-                  </Button>
+              <Transition
+                as={Fragment}
+                enter="duration-150 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Popover.Panel
+                  focus
+                  className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                >
+                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="px-5 pt-4 flex items-center justify-between">
+                      <div>
+                        <img
+                          className="h-8 w-auto"
+                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="-mr-2">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 outline-none">
+                          <span className="sr-only">Close main menu</span>
+                          <IoMdClose className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    <div className="px-2 pt-2 pb-3 space-y-1">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                    <a
+                      href="#"
+                      className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    >
+                      Log in
+                    </a>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+
+            <section className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 lg:h-screen">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">Innovationen nutzen</span>{" "}
+                  <span className="block text-blue-600 xl:inline">
+                    erfolgreich umsetzen
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  Bei <b>Elbe - Technik</b> bieten wir maßgeschneiderte
+                  Techniklösungen für Ihr Unternehmen. Kontaktieren Sie uns per
+                  Telefon oder E-Mail und profitieren Sie von unserer
+                  umfassenden Beratung und Unterstützung. Setzen Sie Ihre
+                  Projekte mit modernster Technik erfolgreich um!
+                </p>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="rounded-md shadow">
+                    <motion.div
+                      className="box"
+                      whileHover={{ scale: 1.1 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 20,
+                      }}
+                    >
+                      <Button className="w-full flex items-center justify-center px-8 py-6 text-base font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 md:py-8 md:text-lg md:px-10">
+                        Mehr Erfahren
+                      </Button>
+                    </motion.div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </main>
+            </section>
+          </div>
         </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 w-full lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt=""
-        />
-      </div>
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 w-full lg:w-1/2">
+          <img
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            alt=""
+          />
+        </div>
+      </main>
+
+      {/*Company*/}
+      <section className="bg-black pt-[85vh] pb-[85vh] relative text-[#fafafa]">
+        <ParallaxText baseVelocity={-5}>Framer Motion</ParallaxText>
+        <ParallaxText baseVelocity={5}>Scroll velocity</ParallaxText>
+      </section>
     </div>
   );
 };
