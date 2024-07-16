46fdbf5173b64adca6661077dc212b03d08e7c4f
Jonaskop44 <jonas@codeflexx.com>
2024-07-16 22:24:20 +0200

    changes

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
                 Innovationen nutzen {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 -z-10 before:h-3 before:w-full before:bg-sky-500/50 dark:before:bg-sky-500/50">
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-sky-500/50 dark:before:bg-sky-500/50">
                   erfolgreich umsetzen
                 </span>
               </h1>
@@ -54,7 +54,7 @@ const Hero = () => {
 
             <div className="animate_right hidden md:w-1/2 lg:block">
               <div className="relative 2xl:-mr-7.5">
-                <div className="relative aspect-[700/444] w-full -z-10">
+                <div className="relative aspect-[700/444] w-full">
                   <Image
                     className="shadow-solid-l dark:hidden"
                     src="/images/blog/blog-big.png"
