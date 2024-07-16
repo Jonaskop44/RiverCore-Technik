1aa4de86313fadb47a2e3166f15fe81ef781f0c0
Jonaskop44 <jonas@codeflexx.com>
2024-07-16 21:49:41 +0200

    changes

---
+++ web/src/components/Reviews/reviewsData.ts
@@ -0,0 +1,102 @@
+import image1 from "@/public/images/user/user-01.png";
+import image2 from "@/public/images/user/user-02.png";
+import { Reviews } from "@/types/reviews";
+
+export const reviewsData: Reviews[] = [
+  {
+    id: 1,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 2,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 3,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 4,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 5,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 6,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 7,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 8,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 9,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 10,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 11,
+    name: "Devid Smith",
+    designation: "Founter @democompany",
+    image: image1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+  {
+    id: 12,
+    name: "Jhon Abraham",
+    designation: "Founter @democompany",
+    image: image2,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+  },
+];
---
+++ web/src/types/reviews.ts
@@ -0,0 +1,8 @@
+export type Reviews = {
+  id: number;
+  name: string;
+  destination?: string;
+  image: any;
+  content: string;
+  designation: string;
+};
