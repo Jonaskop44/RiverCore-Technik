80a941f6b4244d2b9c961bd993a153e55dea6025
Jonaskop44 <jonas@codeflexx.com>
2024-07-16 12:48:30 +0200

    changes

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
                 Innovationen nutzen {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 -z-10 before:h-3 before:w-full before:bg-sky-500/50 dark:before:bg-bg-sky-500/50">
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 -z-10 before:h-3 before:w-full before:bg-sky-500/50 dark:before:bg-sky-500/50">
                   erfolgreich umsetzen
                 </span>
               </h1>
