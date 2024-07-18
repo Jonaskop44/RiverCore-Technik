5ae959a271883a18501802818b539dc7f20e9f5e
Jonaskop44 <jonas@codeflexx.com>
2024-07-18 08:14:25 +0200

    changes

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -10,12 +10,12 @@ const Hero = () => {
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
+            <div className="md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
                 Elbe - Technik
               </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Innovationen nutzen {"   "}
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Innovationen nutzen{" "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-sky-500/50 dark:before:bg-sky-500/50">
                   erfolgreich umsetzen
                 </span>
@@ -52,7 +52,7 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className="animate_right hidden md:w-1/2 lg:block">
+            <div className="animate_right hidden md:w-1/2 md:block">
               <div className="relative 2xl:-mr-7.5">
                 <div className="relative aspect-[700/444] w-full">
                   <Image
