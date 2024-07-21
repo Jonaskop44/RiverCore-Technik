d23a40d3b7d67be3e9424b57e817cc8ea62b1f04
Jonaskop44 <jonas@codeflexx.com>
2024-07-21 23:51:21 +0200

    changes

--- web/src/components/FeaturesTab/featuresTabData.ts
+++ web/src/components/FeaturesTab/featuresTabData.ts
@@ -7,7 +7,7 @@ const featuresTabData: FeatureTab[] = [
     desc1: `Jenny Mustermann bietet umfassende Beratung und Unterstützung für alle Ihre Netzwerk- und IT-Bedürfnisse. Von der Einrichtung sicherer Netzwerke bis zur Wartung von IT-Systemen steht sie Ihnen zur Seite.`,
     desc2: `Mit ihrer Expertise sorgt Jenny dafür, dass Ihre IT-Infrastruktur stets auf dem neuesten Stand ist und reibungslos funktioniert. Vertrauen Sie auf ihre langjährige Erfahrung im Bereich Netzwerktechnologie.`,
     image: "/images/features/features-light-02.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/features-dark-01.png",
   },
   {
     id: "tabTwo",
@@ -15,7 +15,7 @@ const featuresTabData: FeatureTab[] = [
     desc1: `Julian Mustermann ist Ihr Experte für Kassensysteme. Er unterstützt Sie bei der Auswahl, Implementierung und Wartung moderner Kassensysteme, die Ihren Geschäftsablauf effizienter gestalten.`,
     desc2: `Julian hilft Ihnen, die richtige Lösung für Ihre Bedürfnisse zu finden und sicherzustellen, dass Ihre Kassensysteme reibungslos funktionieren. Mit ihm haben Sie einen zuverlässigen Partner an Ihrer Seite.`,
     image: "/images/features/features-light-03.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/features-dark-01.png",
   },
   {
     id: "tabThree",
@@ -23,7 +23,7 @@ const featuresTabData: FeatureTab[] = [
     desc1: `Marisa Mustermann ist Ihre Ansprechpartnerin für alles rund um Drucker und Drucklösungen. Sie berät Sie bei der Auswahl der besten Druckgeräte und sorgt für deren optimale Integration in Ihre Arbeitsprozesse.`,
     desc2: `Marisa bietet Ihnen maßgeschneiderte Lösungen für Ihre Druckanforderungen und steht Ihnen bei Wartung und Support zur Verfügung. Mit ihrer Hilfe bleibt Ihr Druckerpark stets einsatzbereit.`,
     image: "/images/features/features-light-02.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/features-dark-01.png",
   },
 ];
 
