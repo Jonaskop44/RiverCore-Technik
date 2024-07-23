3ea9d8fcb3d1e793256b97ec3278417e8a7e40c3
Jonaskop44 <jonas@codeflexx.com>
2024-07-23 23:27:02 +0200

    changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
