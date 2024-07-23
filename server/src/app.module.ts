1979da288e6a9d372287401fb3d7d176b5b6780f
Jonaskop44 <jonas@codeflexx.com>
2024-07-23 23:25:19 +0200

    changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), UserModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
---
+++ server/src/user/dto/user.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly role: string;
+}
