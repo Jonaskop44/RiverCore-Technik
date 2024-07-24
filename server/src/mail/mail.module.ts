0e9f7cc2c3742be622fb366c84a31d41b8eb4a7f
Jonaskop44 <jonas@codeflexx.com>
2024-07-24 15:31:46 +0200

    changes

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -1,4 +1,35 @@
 import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailController } from './mail.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
-@Module({})
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.mailtrap.io',
+        secure: false,
+        auth: {
+          user: 'your-username',
+          pass: 'your-password',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  controllers: [MailController],
+  providers: [MailService],
+  exports: [MailService],
+})
 export class MailModule {}
--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -1,4 +1,23 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'types/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(user: User, token: string) {
+    const url = `http://localhost:3000/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      //from: '"Support Team" <support@example.com>',
+      subject: 'Elbe Technik KG - Email Adresse bestätigen',
+      template: './confirmation',
+      context: {
+        name: user.name,
+        url,
+      },
+    });
+  }
+}
