640ddc035959d0a2f9e45e1d9c6358215a054bdf
Jonaskop44 <jonas@codeflexx.com>
2024-07-24 08:58:11 +0200

    changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, AuthModule],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dto/auth.dto';
+import { compare } from 'bcrypt';
 
 const EXPIRE_TIME = 1000 * 60 * 60 * 24;
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private userService: UserService) {}
 
-  async validateUser(dto: LoginDto) {}
+  async validateUser(dto: LoginDto) {
+    const user = await this.userService.getUserByEmail(dto.email);
+    if (user && (await compare(dto.password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
 }
