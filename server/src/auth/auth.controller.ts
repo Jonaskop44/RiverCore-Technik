0f60ac116880d78ac5b468607ce53ab58a9f8487
Jonaskop44 <jonas@codeflexx.com>
2024-07-24 08:04:08 +0200

    changes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
-    @Post('register')
+  @Post('register')
+  async registerUser(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
 }
