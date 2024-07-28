4251d27bbe26fe3509b47d3325d29709dac67e2d
Jonaskop44 <jonas@codeflexx.com>
2024-07-28 19:26:13 +0200

    changes

--- web/src/api/auth/login.ts
+++ web/src/api/auth/login.ts
@@ -7,24 +7,6 @@ export default class Login {
   constructor() {}
 
   public async post(user: User) {
-    // try {
-    //   const response = await axios.post(`${Constants.API_BASE}/auth/login`, {
-    //     email: user.email,
-    //     password: user.password,
-    //   });
-
-    //   if (response.status !== 201)
-    //     return { status: false, message: "Invalid Credentials" };
-    //   const data = await response.data;
-
-    //   const { setUser } = useUserStore.getState();
-    //   setUser(data);
-
-    //   return { status: true, data: data, message: "Login sucessfully" };
-    // } catch (err) {
-    //   return { status: false, message: "Something went wrong!" };
-    // }
-
     return axios
       .post(`${Constants.API_BASE}/auth/login`, {
         email: user.email,
