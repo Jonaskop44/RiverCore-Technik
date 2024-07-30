fd0cb1eadba2d67309d613b983c116a27f321537
Jonaskop44 <jonas@codeflexx.com>
2024-07-30 14:07:35 +0200

    changes

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -7,40 +7,52 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserConfirmation(user: User, token: string) {
-    await this.mailerService.sendMail({
-      to: user.email,
-      //from: '"Support Team" <support@example.com>',
-      subject: 'Bestätigungscode für das Elbe-Technik-Konto',
-      template: './ActivateAccount',
-      context: {
-        name: user.firstName,
-        email: user.email,
-        code: token,
-      },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: user.email,
+        //from: '"Support Team" <support@example.com>',
+        subject: 'Bestätigungscode für das Elbe-Technik-Konto',
+        template: './ActivateAccount',
+        context: {
+          name: user.firstName,
+          email: user.email,
+          code: token,
+        },
+      });
+    } catch (error) {
+      console.log('Error sending user confirmation email');
+    }
   }
 
   async sendPasswordReset(user: User, token: string) {
-    await this.mailerService.sendMail({
-      to: user.email,
-      //from: '"Support Team" <support@example.com>',
-      subject: 'Zurücksetzung des Kennworts für das Elbe-Technik-Konto',
-      template: './ResetPassword',
-      context: {
-        name: user.firstName,
-        email: user.email,
-        code: token,
-      },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: user.email,
+        //from: '"Support Team" <support@example.com>',
+        subject: 'Zurücksetzung des Kennworts für das Elbe-Technik-Konto',
+        template: './ResetPassword',
+        context: {
+          name: user.firstName,
+          email: user.email,
+          code: token,
+        },
+      });
+    } catch (error) {
+      console.log('Error sending password reset email', error);
+    }
   }
 
   async sendNewsletterActivation(email: string) {
-    await this.mailerService.sendMail({
-      to: email,
-      //from: '"Support Team" <support@example.com>',
-      subject: 'Angemeldet für den Elbe-Technik-Newsletter',
-      template: './ConfirmNewsletter',
-      context: {},
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        //from: '"Support Team" <support@example.com>',
+        subject: 'Angemeldet für den Elbe-Technik-Newsletter',
+        template: './ConfirmNewsletter',
+        context: {},
+      });
+    } catch (error) {
+      console.log('Error sending newsletter activation email', error);
+    }
   }
 }
