15e2c06e55aec6563c1b195f45241a8614f4cf33
Jonaskop44 <jonas@codeflexx.com>
2024-07-31 23:47:40 +0200

    changes

--- web/src/data/userStore.ts
+++ web/src/data/userStore.ts
@@ -15,23 +15,21 @@ export const useUserStore = create<UserState>((set) => ({
   setUser: (user) => set({ user }),
   fetchUser: async () => {
     const token = Cookies.get("accessToken");
-    if (!token) {
-      throw new Error("No token found");
+    if (token) {
+      await axios
+        .get(`${Constants.API_BASE}/user/token/data/${token}`)
+        .then((response) => {
+          if (response.status !== 200) {
+            return { status: false, data: null, message: "User not found" };
+          }
+
+          const data = response.data;
+          set({ user: data });
+        })
+        .catch((error) => {
+          set({ user: null });
+          return { status: false, data: null, message: "Something went wrong" };
+        });
     }
-
-    await axios
-      .get(`${Constants.API_BASE}/user/token/data/${token}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          return { status: false, data: null, message: "User not found" };
-        }
-
-        const data = response.data;
-        set({ user: data });
-      })
-      .catch((error) => {
-        set({ user: null });
-        return { status: false, data: null, message: "Something went wrong" };
-      });
   },
 }));
