0d75ab89e5f2d3af25ee1f3b001e66ae23df73ac
Jonaskop44 <jonas@codeflexx.com>
2024-08-06 17:24:23 +0200

    changes

--- web/src/app/dashboard/components/Admin/Users/EditUserModal.tsx
+++ web/src/app/dashboard/components/Admin/Users/EditUserModal.tsx
@@ -7,7 +7,9 @@ import {
   ModalFooter,
   Button,
   Input,
-  Link,
+  Select,
+  SelectItem,
+  Checkbox,
 } from "@nextui-org/react";
 import { User } from "@/types/user";
 
@@ -18,6 +20,17 @@ interface EditUserModalProps {
   data: User;
 }
 
+const designation = [
+  { key: "COMPANY", label: "Unternehmen" },
+  { key: "PERSON", label: "Privatperson" },
+];
+
+const roles = [
+  { key: "ADMIN", label: "Administrator" },
+  { key: "MODERATOR", label: "Moderator" },
+  { key: "USER", label: "Benutzer" },
+];
+
 const EditUserModal: React.FC<EditUserModalProps> = ({
   isOpen,
   onOpen,
@@ -25,75 +38,108 @@ const EditUserModal: React.FC<EditUserModalProps> = ({
   data,
 }) => {
   return (
-    <>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={() => {
-          onOpenChange(false);
-        }}
-        placement="center"
-        backdrop="blur"
-        className="z-99999"
-        motionProps={{
-          variants: {
-            enter: {
-              y: 0,
-              opacity: 1,
-              transition: {
-                duration: 0.3,
-                ease: "easeOut",
-              },
-            },
-            exit: {
-              y: -20,
-              opacity: 0,
-              transition: {
-                duration: 0.2,
-                ease: "easeIn",
-              },
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={() => onOpenChange(false)}
+      placement="center"
+      backdrop="blur"
+      className="z-99999"
+      motionProps={{
+        variants: {
+          enter: {
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.3,
+              ease: "easeOut",
             },
           },
-        }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1 dark:text-white">
-                Konto aktivieren
-              </ModalHeader>
-              <ModalBody>
-                <p>
-                  Um Ihr Konto zu aktivieren geben Sie bitte den 15-minütigen
-                  gültigen Bestätigungscode ein, den wir an{" "}
-                  <span className="font-semibold">data.email</span> gesendet
-                  haben.
-                </p>
+          exit: {
+            y: -20,
+            opacity: 0,
+            transition: {
+              duration: 0.2,
+              ease: "easeIn",
+            },
+          },
+        },
+      }}
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1 dark:text-white">
+              Benutzer bearbeiten
+            </ModalHeader>
+            <ModalBody>
+              <p>
+                Um die Details für den Benutzer{" "}
+                <b>
+                  {data.firstName} {data.lastName}{" "}
+                </b>
+                zu bearbeiten, passen Sie bitte die folgenden Informationen an:
+              </p>
+              <Input
+                label="Vorname"
+                defaultValue={data.firstName}
+                isRequired
+                errorMessage="Bitte geben Sie den Vornamen ein"
+                variant="underlined"
+              />
+              <Input
+                label="Nachname"
+                defaultValue={data.lastName}
+                isRequired
+                errorMessage="Bitte geben Sie den Nachnamen ein"
+                variant="underlined"
+              />
+              <Input
+                label="E-Mail"
+                defaultValue={data.email}
+                isRequired
+                errorMessage="Bitte geben Sie eine gültige E-Mail-Adresse ein"
+                variant="underlined"
+              />
+              <Select label="Rolle" variant="underlined">
+                {roles.map((item) => (
+                  <SelectItem key={item.key}>{item.label}</SelectItem>
+                ))}
+              </Select>
+              <Select
+                label="Anrede"
+                variant="underlined"
+                defaultSelectedKeys={"all"}
+              >
+                {designation.map((item) => (
+                  <SelectItem key={item.key}>{item.label}</SelectItem>
+                ))}
+              </Select>
+              {data.designation === "COMPANY" && (
                 <Input
-                  label="Bestätigungscode"
+                  label="Unternehmen"
+                  defaultValue={data.companyName}
                   isRequired
-                  errorMessage="Bitte geben Sie den Bestätigungscode ein"
+                  errorMessage="Bitte geben Sie den Firmennamen ein"
                   variant="underlined"
                 />
-                <div className="flex py-2 px-1 justify-between">
-                  <Link color="primary" size="sm" className="cursor-pointer">
-                    Bestätigungscode erneut senden
-                  </Link>
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button
-                  color="primary"
-                  onPress={() => console.log("Test")}
-                  className="cursor-pointer"
-                >
-                  Bestätigen
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+              )}
+              <div className="flex py-2 px-1 justify-between">
+                <Checkbox isSelected={data.activated}>Aktiviert</Checkbox>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                color="primary"
+                onPress={() => console.log("Daten gespeichert")}
+                className="cursor-pointer"
+              >
+                Speichern
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   );
 };
 
--- web/src/app/dashboard/components/Admin/Users/UserCards.tsx
+++ web/src/app/dashboard/components/Admin/Users/UserCards.tsx
@@ -1,67 +1,93 @@
 import { User } from "@/types/user";
 import { MdDelete } from "react-icons/md";
 import { RiEdit2Fill } from "react-icons/ri";
+import EditUserModal from "./EditUserModal";
+import { useEffect, useState } from "react";
+import { useDisclosure } from "@nextui-org/react";
 
 interface UserCardProps {
   data: User[];
 }
 
 const UserCards: React.FC<UserCardProps> = ({ data }) => {
+  const [user, setUser] = useState<User>(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  useEffect(() => {
+    console.log(user);
+  });
+
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
-    >
-      {data.map((person) => (
-        <li
-          key={person.email}
-          className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
-        >
-          <div className="w-full flex items-center justify-between p-6 space-x-6">
-            <div className="flex-1 truncate">
-              <div className="flex items-center space-x-3">
-                <h3 className="text-gray-900 text-sm font-medium truncate">
-                  {person.firstName} {person.lastName}
-                </h3>
-                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                  {person.designation}
-                </span>
+    <>
+      <div>
+        <EditUserModal
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onOpenChange={onOpenChange}
+          data={user}
+        />
+      </div>
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+      >
+        {data.map((person) => (
+          <li
+            key={person.email}
+            className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+          >
+            <div className="w-full flex items-center justify-between p-6 space-x-6">
+              <div className="flex-1 truncate">
+                <div className="flex items-center space-x-3">
+                  <h3 className="text-gray-900 text-sm font-medium truncate">
+                    {person.firstName} {person.lastName}
+                  </h3>
+                  <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                    {person.designation}
+                  </span>
+                </div>
+                <p className="mt-1 text-gray-500 text-sm truncate">
+                  {person.email}
+                </p>
               </div>
-              <p className="mt-1 text-gray-500 text-sm truncate">
-                {person.email}
-              </p>
+              <img
+                className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                alt=""
+              />
             </div>
-            <img
-              className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-              alt=""
-            />
-          </div>
-          <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
-              <div className="w-0 flex-1 flex">
-                <button className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
-                  <MdDelete
-                    className="w-5 h-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-3">Löschen</span>
-                </button>
-              </div>
-              <div className="-ml-px w-0 flex-1 flex">
-                <button className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-                  <RiEdit2Fill
-                    className="w-5 h-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-3">Bearbeiten</span>
-                </button>
+            <div>
+              <div className="-mt-px flex divide-x divide-gray-200">
+                <div className="w-0 flex-1 flex">
+                  <button className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                    <MdDelete
+                      className="w-5 h-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-3">Löschen</span>
+                  </button>
+                </div>
+                <div className="-ml-px w-0 flex-1 flex">
+                  <button
+                    className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                    onClick={() => {
+                      setUser(person);
+                      onOpen();
+                    }}
+                  >
+                    <RiEdit2Fill
+                      className="w-5 h-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-3">Bearbeiten</span>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
