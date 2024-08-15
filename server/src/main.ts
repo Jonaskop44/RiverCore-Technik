c0df5cb5a115791c8e4733c73581e9eb147bef9f
Jonaskop44 <jonas@codeflexx.com>
2024-08-15 20:43:54 +0200

    changes

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,24 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions: CorsOptions = {
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+  // const corsOptions: CorsOptions = {
+  //   origin: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  //   credentials: true,
+  // };
+
+  const allowedOrigins = ['http://localhost:3000'];
+
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200,
   };
 
   app.enableCors();
