d5c48330c928a0e401edd844d7c3ea2d33b6a408
Jonaskop44 <jonas@codeflexx.com>
2024-08-18 17:49:34 +0200

    changes

--- web/src/app/dashboard/user/support/printer/page.tsx
+++ web/src/app/dashboard/user/support/printer/page.tsx
@@ -3,7 +3,7 @@
 import NewChatModal from "@/app/dashboard/components/User/Support/NewChatModal";
 import { useUserStore } from "@/data/userStore";
 import { Avatar, Button, Chip, Input, useDisclosure } from "@nextui-org/react";
-import Cookies from "js-cookie";
+import Cookies, { set } from "js-cookie";
 import { useEffect, useRef, useState } from "react";
 import { FiPaperclip } from "react-icons/fi";
 import { IoIosSend } from "react-icons/io";
@@ -16,7 +16,7 @@ const socket = io("ws://localhost:3001", {
 });
 
 const PrinterSupport = () => {
-  const { user, profilePicture } = useUserStore();
+  const { profilePicture } = useUserStore();
   const [chats, setChats] = useState([]);
   const [messages, setMessages] = useState([]);
   const [selectedChat, setSelectedChat] = useState(null);
@@ -116,7 +116,7 @@ const PrinterSupport = () => {
         setTitle={setNewChatTitle}
         handleCreateChat={createChat}
       />
-      <div className="flex bg-white rounded-lg max-h-[500px] h-screen">
+      <div className="flex bg-white rounded-lg max-h-[600px] h-screen">
         <aside className="w-1/4 border-r">
           <div className="p-4">
             <h2 className="text-lg font-semibold text-black">
@@ -131,9 +131,9 @@ const PrinterSupport = () => {
                 Neue Konversation
               </Button>
             </div>
-            <div className="space-y-4">
+            <ul className="space-y-4">
               {chats.map((chat) => (
-                <div
+                <li
                   key={chat.id}
                   className="flex items-center space-x-3 cursor-pointer"
                   onClick={() => joinChat(chat.id)}
@@ -143,92 +143,44 @@ const PrinterSupport = () => {
                     <p className="font-medium">{chat.title}</p>
                     <p className="text-sm text-muted-foreground">Name</p>
                   </div>
-                  {/* {contact.count && (
-                  // <Badge variant="secondary">{contact.count}</Badge>
-                  <Chip color="primary">{contact.count}</Chip>
-                )} */}
                   <Chip color="primary">10</Chip>
                   <span
                     className={`h-2 w-2 rounded-full ${
                       "online" === "online" ? "bg-green-500" : "bg-red-500"
                     }`}
                   />
-                </div>
+                </li>
               ))}
-            </div>
+            </ul>
           </div>
         </aside>
         <main className="flex-1 flex flex-col">
           <header className="flex items-center p-4 border-b">
             <Avatar src={profilePicture} />
             <div className="ml-3">
-              <h2 className="text-lg font-semibold">Henry Dholi</h2>
-              <p className="text-sm text-muted-foreground">
-                {typingUsers.length > 0 && (
-                  <div>
-                    {typingUsers.map((user) => (
-                      <span key={user.id}>
-                        {user.firstName} {user.lastName} schreibt ...
-                      </span>
-                    ))}
-                  </div>
-                )}
-              </p>
+              {selectedChat ? (
+                <div>
+                  <h2 className="text-lg font-semibold">Leon Maier</h2>
+                  <p className="text-sm text-muted-foreground">
+                    {typingUsers.length > 0 && (
+                      <div>
+                        {typingUsers.map((user) => (
+                          <span key={user.id}>
+                            {user.firstName} {user.lastName} schreibt ...
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                  </p>
+                </div>
+              ) : (
+                <h2 className="text-lg font-semibold">
+                  Wähle eine Konversation aus
+                </h2>
+              )}
             </div>
           </header>
           <div className="flex-1 p-4 overflow-y-auto">
-            {/* {[
-            {
-              name: "Andri Thomas",
-              message:
-                "I want to make an appointment tomorrow from 2:00 to 5:00pm?",
-              time: "1:55pm",
-              from: "them",
-            },
-            {
-              name: "Henry Dholi",
-              message:
-                "Hello, Thomas! I will check the schedule and inform you",
-              time: "1:58pm",
-              from: "me",
-            },
-            {
-              name: "Andri Thomas",
-              message: "Ok, Thanks for your reply.",
-              time: "1:59pm",
-              from: "them",
-            },
-            {
-              name: "Henry Dholi",
-              message: "You are welcome!",
-              time: "2:00pm",
-              from: "me",
-            },
-            {
-              name: "Andri Thomas",
-              message:
-                "I want to make an appointment tomorrow from 2:00 to 5:00pm?",
-              time: "2:01pm",
-              from: "them",
-            },
-          ].map((chat, index) => (
-            <div
-              key={index}
-              className={`flex ${chat.from === "me" ? "justify-end" : ""} mb-4`}
-            >
-              <div
-                className={`max-w-xs p-3 rounded-lg ${
-                  chat.from === "me" ? "bg-blue-100" : "bg-gray-100"
-                }`}
-              >
-                <p>{chat.message}</p>
-                <span className="block mt-1 text-xs text-muted-foreground">
-                  {chat.time}
-                </span>
-              </div>
-            </div>
-          ))} */}
-
             {selectedChat ? (
               <>
                 <ul>
