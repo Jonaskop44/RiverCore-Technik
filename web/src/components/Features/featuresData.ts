a1103aa00ae9ef8a0eae16e9199ded59264bb6cf
Jonaskop44 <jonas@codeflexx.com>
2024-09-02 16:35:02 +0200

    changes

--- web/src/components/Features/featuresData.ts
+++ web/src/components/Features/featuresData.ts
@@ -5,40 +5,38 @@ const featuresData: Feature[] = [
     id: 1,
     icon: "/images/icon/icon-01.svg",
     title: "Apple for Business",
-    description: "Die Power von Apple. Bei der Arbeit.",
+    description: "Die besten Apple-Produkte für Ihr Unternehmen.",
   },
   {
     id: 2,
     icon: "/images/icon/icon-02.svg",
     title: "Modern Workplace",
-    description: "Eine Rundum-Lösung für den Arbeitsplatz der Zukunft.",
+    description: "Arbeiten Sie effizienter und sicherer mit Microsoft 365.",
   },
   {
     id: 3,
     icon: "/images/icon/icon-03.svg",
     title: "Digital Signage",
-    description:
-      "Begeistern Sie Ihre Kunden mit interaktiven visuellen Inhalten, die verkaufen.",
+    description: "Präsentieren Sie Ihre Inhalte auf modernste Art und Weise.",
   },
   {
     id: 4,
     icon: "/images/icon/icon-04.svg",
     title: "Microsoft 365",
-    description: "Die innovative Office-Komplettlösung für alle Unternehmen.",
+    description: "Die besten Microsoft-Produkte für Ihr Unternehmen.",
   },
   {
     id: 5,
     icon: "/images/icon/icon-05.svg",
     title: "Server & Netzwerk",
-    description:
-      "Höchste Sicherheit und stabile Netzwerke sind das Um un Auf Ihres Unternehmens.",
+    description: "Wir bieten Ihnen die besten Lösungen für Ihr Netzwerk.",
   },
   {
     id: 6,
     icon: "/images/icon/icon-06.svg",
     title: "Smart Home & Smart Office",
     description:
-      "Steuern Sie Ihr Zuhause oder Ihr Office von überall auf der Welt.",
+      "Automatisieren Sie Ihr Zuhause oder Büro und sparen Sie Zeit und Geld.",
   },
 ];
 
