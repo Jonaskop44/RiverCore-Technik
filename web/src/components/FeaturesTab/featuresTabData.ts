fecd2ef7309b6a52ec1dffaba8c2bca3aa5beef9
Jonaskop44 <jonas@codeflexx.com>
2024-09-02 16:53:07 +0200

    changes

--- web/src/components/FeaturesTab/featuresTabData.ts
+++ web/src/components/FeaturesTab/featuresTabData.ts
@@ -6,7 +6,7 @@ const featuresTabData: FeatureTab[] = [
     title: "Nina Müller",
     desc1: `Nina Müller bietet umfassende Beratung und Unterstützung für alle Ihre Netzwerk- und IT-Bedürfnisse. Von der Einrichtung sicherer Netzwerke bis zur Wartung von IT-Systemen steht sie Ihnen zur Seite.`,
     desc2: `Mit ihrer Expertise sorgt Nina dafür, dass Ihre IT-Infrastruktur stets auf dem neuesten Stand ist und reibungslos funktioniert. Vertrauen Sie auf ihre langjährige Erfahrung im Bereich Netzwerktechnologie.`,
-    image: "/images/features/features-light-02.png",
+    image: "/images/features/features-light-01.png",
     imageDark: "/images/features/features-dark-01.png",
   },
   {
@@ -14,7 +14,7 @@ const featuresTabData: FeatureTab[] = [
     title: "John Doe",
     desc1: `John Doe ist Ihr Experte für Kassensysteme. Er unterstützt Sie bei der Auswahl, Implementierung und Wartung moderner Kassensysteme, die Ihren Geschäftsablauf effizienter gestalten.`,
     desc2: `John hilft Ihnen, die richtige Lösung für Ihre Bedürfnisse zu finden und sicherzustellen, dass Ihre Kassensysteme reibungslos funktionieren. Mit ihm haben Sie einen zuverlässigen Partner an Ihrer Seite.`,
-    image: "/images/features/features-light-03.png",
+    image: "/images/features/features-light-01.png",
     imageDark: "/images/features/features-dark-01.png",
   },
   {
@@ -22,7 +22,7 @@ const featuresTabData: FeatureTab[] = [
     title: "Kim Schmidt",
     desc1: `Kim Schmidt ist Ihre Ansprechpartnerin für alles rund um Drucker und Drucklösungen. Sie berät Sie bei der Auswahl der besten Druckgeräte und sorgt für deren optimale Integration in Ihre Arbeitsprozesse.`,
     desc2: `Kim bietet Ihnen maßgeschneiderte Lösungen für Ihre Druckanforderungen und steht Ihnen bei Wartung und Support zur Verfügung. Mit ihrer Hilfe bleibt Ihr Druckerpark stets einsatzbereit.`,
-    image: "/images/features/features-light-02.png",
+    image: "/images/features/features-light-01.png",
     imageDark: "/images/features/features-dark-01.png",
   },
 ];
