bb922a5db850ce51a2e88c78976fea092b3c612f
Jonaskop44 <jonas@codeflexx.com>
2024-09-02 15:58:48 +0200

    changes

--- web/src/app/team/page.tsx
+++ web/src/app/team/page.tsx
@@ -13,8 +13,8 @@ const people = [
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
     bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
-    email: "gert.schwanter@elbe.at",
-    number: "Tel.: +43 4242/51 115",
+    email: "example@example.com",
+    number: "Tel.: +34 123 456 789",
   },
   {
     name: "Leslie Alexander",
@@ -22,8 +22,8 @@ const people = [
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
     bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
-    email: "gert.schwanter@elbe.at",
-    number: "Tel.: +43 4242/51 115",
+    email: "example@example.com",
+    number: "Tel.: +34 123 456 789",
   },
   {
     name: "Leslie Alexander",
@@ -31,8 +31,8 @@ const people = [
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
     bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
-    email: "gert.schwanter@elbe.at",
-    number: "Tel.: +43 4242/51 115",
+    email: "example@example.com",
+    number: "Tel.: +34 123 456 789",
   },
 ];
 
